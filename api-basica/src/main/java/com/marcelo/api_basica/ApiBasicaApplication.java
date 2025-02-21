d8a279dab0b03d837d3d8f0aaa972bfcae1f06e0
marcelosrg <marcelohrs2005@gmail.com>
2025-02-20 21:35:22 -0300

    chore: setup da aplicação

---
+++ api-basica/src/main/java/com/marcelo/api_basica/ApiBasicaApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.api_basica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBasicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBasicaApplication.class, args);
+	}
+
+}
---
+++ api-basica/src/test/java/com/marcelo/api_basica/ApiBasicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo.api_basica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBasicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
